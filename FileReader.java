6a42891924c1121782c3e99495798f6fc02a0a76
Tierra Sharae <44881884+TierraSharae@users.noreply.github.com>
2019-04-24 16:03:37 -0400

    eliminated lines arrayList, still not done

--- FileReader.java
+++ FileReader.java
@@ -7,13 +7,13 @@ import java.util.Scanner;
 public class FileReader {
 
     private String filename;
-    private ArrayList<String> lines;
+//    private ArrayList<String> lines;
     private HashMap <String, Integer> words;
     private HashMap<String, HashMap<String, Integer>> successors;
 
     public FileReader (String fn) throws FileNotFoundException {
 	filename = fn;
-	lines = new ArrayList<>();
+//	lines = new ArrayList<>();
 	words = new HashMap <String, Integer>();
 	successors = new HashMap<String, HashMap<String, Integer>> ();
 	readFile();
@@ -24,50 +24,48 @@ public class FileReader {
 	return words;
     }
 
-    public ArrayList<String> getLines(){
-	return lines;
-    }
 
     private void readFile() throws FileNotFoundException {
 
 	Scanner sc = new Scanner(new File(filename));
 
-	//	String w;
+		String w;
 
-	while(sc.hasNextLine()) {
-	    String line = sc.nextLine();
-	    lines.add(line.toLowerCase());
-	}
-	sc.close();
+//	while(sc.hasNext()) {
+//	    String line = sc.next();
+//	    lines.add(line.toLowerCase());
+//	}
+//	sc.close();
+//
+//	for(String line : lines) {
+//	    String[] tokens = line.split(" ");
+//	    for(String token : tokens){
+//		token = token.trim();
+//		token = token.replaceAll("[^a-zA-Z0-9']", "");
+//		if(token.equals("")) continue;
+//		if (words.containsKey(token)) {
+//		    words.put(token, words.get(token) + 1);
+//		} else {
+//		    words.put(token, 1);
+//		}
+//	    }
+//	}
 
-	for(String line : lines) {
-	    String[] tokens = line.split(" ");
-	    for(String token : tokens){
-		token = token.trim();
-		token = token.replaceAll("[^a-zA-Z0-9']", "");
-		if(token.equals("")) continue;
-		if (words.containsKey(token)) {
-		    words.put(token, words.get(token) + 1);
-		} else {
-		    words.put(token, 1);
+
+		while (sc.hasNext()) {
+		    w = sc.next();
+		    w = w.toLowerCase();
+		    w = w.trim();
+		    w = w.replaceAll("[^a-zA-Z0-9']", "");
+		    if (words.containsKey(w)) {
+			words.put(w, words.get(w) + 1);
+		    } else {
+			words.put(w, 1);
+		    }
 		}
-	    }
-	}
-
-
-	//	while (sc.hasNext()) {
-	//	    w = sc.next();
-	//	    w = w.toLowerCase();
-	//
-	//	    if (words.containsKey(w)) {
-	//		words.put(w, words.get(w) + 1);
-	//	    } else {
-	//		words.put(w, 1);
-	//	    }
-	//	}
-	//	
-	//	sc.close();
-	//
+		
+		sc.close();
+	
     }
 
     public void makeFrequencyTable (String word) {
