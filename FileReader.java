ed8ee1efc88fae0ddeb426766479b2d0c10ece73
Tierra Sharae <44881884+TierraSharae@users.noreply.github.com>
2019-04-27 12:15:15 -0400

    should be good to go 💃🏾

--- FileReader.java
+++ FileReader.java
@@ -1,25 +1,30 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class FileReader {
 
     private String filename;
+    private ArrayList<String> lines;
     private HashMap <String, Integer> words;
     private HashMap<String, HashMap<String, Integer>> successors;
+    private HashMap<String, Integer> hm; //this is the map that is within successors
 
     public FileReader (String fn) throws FileNotFoundException {
 	filename = fn;
+	lines = new ArrayList<>();
 	words = new HashMap <String, Integer>();
 	successors = new HashMap<String, HashMap<String, Integer>> ();
 	readFile();
     }
 
+
     public HashMap <String, Integer> getWords(){
 	return words;
     }
-    
+
     public HashMap <String, HashMap<String, Integer>> getSuccessors(){
 	return successors;
     }
@@ -29,17 +34,37 @@ public class FileReader {
 
 	Scanner sc = new Scanner(new File(filename));
 
-	String tok; //the token
-
-
-	while (sc.hasNext()) {
-	    tok = sc.next();
-	    tok = tok.toLowerCase().trim().replaceAll("[^a-zA-Z0-9']", ""); //woohoo!
-	    makeFrequencyTable(tok);
+	while(sc.hasNext()) {
+	    String line = sc.next().replace(".", " . ").replace("!", " ! ").replace("?", " ? ");
+	    //above, we add space around the punctuation, so the punc'n can be considered a "word"
+	    String[] tokens = line.split(" ");
+	    for(String token : tokens){
+		token = token.trim().replaceAll("[^a-zA-Z0-9'.?!]", "");
+		lines.add(token.toLowerCase());
+	    }
 	}
-
 	sc.close();
 
+	for(String line : lines) {
+	    String[] tokens = line.split(" ");
+	    for(String token : tokens){
+		if (isPunctuation(token)) continue;
+		if(token.equals("")) continue;
+		makeFrequencyTable(token);
+	    }
+	}
+
+	for (int i = 0 ; i < lines.size()-1 ; i++) {
+	    if (isPunctuation(lines.get(i+1)) || isPunctuation(lines.get(i))) continue;
+	    //if "previous" or "word" is punctuation, we won't call makeSuccMap() on it
+	    makeSuccessorMap (lines.get(i+1), lines.get(i));
+	}
+    }
+
+    //helper so we don't have to keep saying if x.equals . || ! || ?
+    private boolean isPunctuation (String word) {
+	if (word.equals(".") || word.equals("!") || word.equals("?")) return true;
+	return false;
     }
 
     public void makeFrequencyTable (String word) {
@@ -53,6 +78,18 @@ public class FileReader {
 
     public void makeSuccessorMap (String word, String previous) {
 
+	if (successors.containsKey(previous)) {
+	    hm = successors.get(previous);
+	    if (hm.containsKey(word)) {
+		hm.put(word, hm.get(word)+1);
+	    } else {
+		hm.put(word, 1);
+	    }
+	} else {
+	    hm = new HashMap<String, Integer>();
+	    hm.put(word, 1);
+	    successors.put(previous, hm);
+	}
     }
-
 }
+
--- FileReaderTest.java
+++ FileReaderTest.java
@@ -1,18 +1,34 @@
+import static org.junit.Assert.assertEquals;
+
 import java.io.FileNotFoundException;
 
 import org.junit.Test;
 
 public class FileReaderTest {
     
-    @Test
+    @Test //the string: This is a test file. Repeat some words: test. Test. test file file file! it's so so cool
     public void testMakeFrequencyTable() throws FileNotFoundException {
 	FileReader fr = new FileReader("testfile.txt");
-	System.out.println(fr.getWords());
+	assertEquals("1", fr.getWords().get("this").toString());
+	assertEquals("1", fr.getWords().get("is").toString());
+	assertEquals("1", fr.getWords().get("a").toString());
+	assertEquals("4", fr.getWords().get("test").toString());
+	assertEquals("4", fr.getWords().get("file").toString());
+	assertEquals("1", fr.getWords().get("repeat").toString());
+	assertEquals("1", fr.getWords().get("some").toString());
+	assertEquals("1", fr.getWords().get("words").toString());
+	assertEquals("1", fr.getWords().get("it's").toString());
+	assertEquals("2", fr.getWords().get("so").toString());
+	assertEquals("1", fr.getWords().get("cool").toString());
+	
     }
     
-//    @Test
-//    public void testMakeSuccessorTable() throws FileNotFoundException {
-//	FileReader fr = new FileReader("successor.txt");
-//	System.out.println(fr.getSuccessors());
-//    }
+    @Test //the string: Happy new year! Happy new day? Happy new year, friend! Brand new car.
+    public void testMakeSuccessorTable() throws FileNotFoundException {
+	FileReader fr = new FileReader("successor.txt");
+	assertEquals("{new=3}", fr.getSuccessors().get("happy").toString());
+	assertEquals("{year=2, car=1, day=1}", fr.getSuccessors().get("new").toString());
+	assertEquals("{friend=1}", fr.getSuccessors().get("year").toString());
+	assertEquals("{new=1}", fr.getSuccessors().get("brand").toString());
+    }
 }
