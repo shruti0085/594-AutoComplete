7a29536df0066f499bdaaf6005b3cf47b3e79d49
Ada Lovelace <irebecca@seas.upenn.edu>
2019-04-30 16:35:09 -0400

    Added some new JUnit for more code coverage.  Note: JUnit has been converted to JUnit 4.

--- AutocompleteTest.java
+++ AutocompleteTest.java
@@ -1,20 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 import java.util.HashMap;
 
-import org.junit.jupiter.api.Test;
+import java.util.HashMap;
 
-class AutocompleteTest {
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+public class AutocompleteTest {
+  
 
 	@Test
-	void testAllPredictions() {
+	public void testAllPredictions() {  
+		
 		HashMap<String, Integer> wTable = new HashMap<String, Integer>() ;
 		wTable.put("apple", 5) ;
 		wTable.put("apply", 2) ;
 		wTable.put("juice", 8) ;
 		wTable.put("pie", 7) ;
 		wTable.put("please", 10) ;
-		wTable.put("is", 100) ;
+		wTable.put("is", 100) ;  
 		
 		HashMap<String, Integer> appleSuccessors = new HashMap<String, Integer> () ;
 		appleSuccessors.put("juice", 2) ;
@@ -43,31 +58,33 @@ class AutocompleteTest {
 		successors.put("juice", juiceSuccessors) ;
 		successors.put("please", pleaseSuccessors) ;
 		successors.put("pie", pieSuccessors) ;
-		successors.put("is", isSuccessors) ;		
+		successors.put("is", isSuccessors) ;		   
 		
-		Autocomplete auto = new Autocomplete(wTable, successors) ;
+		Autocomplete auto = new Autocomplete(wTable, successors) ; 
+		
 		
 		String[] resultApp = auto.allPredictions("app") ;
 		String[] wantApp = {"apple", "apply"} ;
-		System.out.println("app");
+		//System.out.println("app");
 		for (int i = 0; i < resultApp.length; i++) {
 			assertEquals(wantApp[i], resultApp[i]) ;
-		}
+		} 
 		
 		resultApp = auto.allPredictions("apple ") ;
 		String[] wantApp2 = {"juice", "pie", "please"} ;
-		System.out.println("apple ");
+		//System.out.println("apple ");
 		for (int i = 0; i < resultApp.length; i++) {
 			assertEquals(wantApp2[i], resultApp[i]) ;
 		}
 		
 		resultApp = auto.allPredictions("apple p") ;
 		String[] wantApp3 = {"pie", "please"} ;
-		System.out.println("apple p");
+		//System.out.println("apple p");
 		for (int i = 0; i < resultApp.length; i++) {
 			assertEquals(wantApp3[i], resultApp[i]) ;
-		}
+		} 
 		
+		//What does this test for?
 		resultApp = auto.allPredictions("apple pie ") ;
 		String[] wantApp4 = {} ;
 		for (int i = 0; i < resultApp.length; i++) {
@@ -80,13 +97,73 @@ class AutocompleteTest {
 			assertEquals(wantApp5[i], resultApp[i]) ;
 		}
 		
-		// test empty input
+		// test empty input, this should fail
 		resultApp = auto.allPredictions("") ;
 		String[] wantApp6 = new String[0] ;
 		for (int i = 0; i < resultApp.length; i++) {
 			assertEquals(wantApp6[i], resultApp[i]) ;
-		}
+		} 
 
 	}
+	
+    /**
+     * @throws IllegalStateException
+     */
+    @Test(expected = IllegalStateException.class) 
+    public void test1of3_Autocomplete_IllegalStateException()
+    {
+
+		HashMap<String, Integer> h1 = new HashMap<String, Integer>();
+		h1.put("apple", 5) ;
+		h1.put("apply", 2) ;    
+   
+		
+		HashMap<String, HashMap<String, Integer>> h2 = null; 
+		
+		Autocomplete a = new Autocomplete(h1, h2);    
+		     
+    } 
+    
+    /**
+     * @throws IllegalStateException
+     */
+    @Test(expected = IllegalStateException.class) 
+    public void test2of3_Autocomplete_IllegalStateException()
+    {
+
+		HashMap<String, Integer> h1 = null;
+		HashMap<String, Integer> appleSuccessors = new HashMap<String, Integer> () ;
+		appleSuccessors.put("juice", 2) ;
+		appleSuccessors.put("pie", 2) ;
+		appleSuccessors.put("please", 1) ;
+		
+		HashMap<String, Integer> applySuccessors = new HashMap<String, Integer> () ;
+		applySuccessors.put("please", 1) ;
+		
+		HashMap<String, Integer> juiceSuccessors = new HashMap<String, Integer> () ;
+		juiceSuccessors.put("is", 3) ;
+		juiceSuccessors.put("please", 4) ;
+		
+		HashMap<String, Integer> pleaseSuccessors = new HashMap<String, Integer> () ;
+		
+		HashMap<String, Integer> pieSuccessors = new HashMap<String, Integer> () ;
+		
+		HashMap<String, Integer> isSuccessors = new HashMap<String, Integer> () ;
+		isSuccessors.put("apple", 60) ;
+		isSuccessors.put("juice", 30) ;
+		isSuccessors.put("pie", 10) ;
+		
+		HashMap<String, HashMap<String, Integer>> successors = new HashMap<>() ;
+		successors.put("apple", appleSuccessors) ;
+		successors.put("apply", applySuccessors) ;
+		successors.put("juice", juiceSuccessors) ;
+		successors.put("please", pleaseSuccessors) ;
+		successors.put("pie", pieSuccessors) ;
+		successors.put("is", isSuccessors) ;
+		
+		Autocomplete a = new Autocomplete(h1, successors);    
+		      
+    } 
+
 
 }
